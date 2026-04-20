7354727830f206fdeff9ad6e593dfce2ec39fa10
Adisai Plomlee <adisai.p@innovationssolution.com>
2026-04-20 10:51:43 +0700

    Refactor code structure for improved readability and maintainability

--- obj/Debug/net9.0/AspnetCoreMvcFull.AssemblyInfo.cs
+++ obj/Debug/net9.0/AspnetCoreMvcFull.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AspnetCoreMvcFull")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c122148242914ae3e73018212804c2712814cf93")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc71cde8bf425268c882b754409db964271ba351")]
 [assembly: System.Reflection.AssemblyProductAttribute("AspnetCoreMvcFull")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AspnetCoreMvcFull")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
