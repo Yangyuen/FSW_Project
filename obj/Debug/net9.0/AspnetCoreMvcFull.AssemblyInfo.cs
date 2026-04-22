6a7a988594162c01e74f71612f073d358d3694a3
Adisai Plomlee <adisai.p@innovationssolution.com>
2026-04-22 13:38:46 +0700

    Add RMBS1:NoPS and RMBS1:PS adjustment views with filtering functionality
    - Created IndexAdjustRMBS1NoPS.cshtml for managing RMBS1:NoPS records.
    - Created IndexAdjustRMBS1PS.cshtml for managing RMBS1:PS records.
    - Implemented date filtering and search functionality for both views.
    - Added responsive table layout to display records with relevant details.
    - Included styles and scripts for date picker and tooltip functionality.

--- obj/Debug/net9.0/AspnetCoreMvcFull.AssemblyInfo.cs
+++ obj/Debug/net9.0/AspnetCoreMvcFull.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AspnetCoreMvcFull")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7a15f629b38db632bc20b775ec9b358e7d331c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+906ce7892f20323f4ecc3b442388641586574e54")]
 [assembly: System.Reflection.AssemblyProductAttribute("AspnetCoreMvcFull")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AspnetCoreMvcFull")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
